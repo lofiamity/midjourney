0b07f44c4eed70f4ec53018dc8cc4933894b49cc
lofiamity <108965582+lofiamity@users.noreply.github.com>
2024-03-07 05:40:17 +0700

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const jsonName = urlParams.get('json');
 
     // Menentukan path menuju file JSON berdasarkan parameter JSON
-    const jsonPath = jsonName ? `/${jsonName}.json` : '../../explore.json';
+    const jsonPath = jsonName ? `/${jsonName}.json` : 'explore';
 
     fetch(jsonPath)
         .then(response => response.json())
